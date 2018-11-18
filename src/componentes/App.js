2450819e1da7525eae8982b280f0256834c5ae06
Melisa <soymelisa@yahoo.com>
2018-11-18 01:40:06 -0600

    resumen y total de la cotización

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Formulario from './Formulario';
+import Resumen from './Resumen';
+import Resultado from './Resultado';
 import {obtenerDiferenciaAnio, calcularMarca, obtenerPlan} from '../helper';
 
 class App extends Component {
@@ -51,7 +53,13 @@ class App extends Component {
         <div className="contenedor-formulario">
         <Formulario
           cotizarSeguro= {this.cotizarSeguro}
-        />
+          />
+          <Resumen 
+            datos={this.state.datos}
+          />
+          <Resultado 
+            resultado={this.state.resultado}
+          />
         </div>
       </div>
     );
